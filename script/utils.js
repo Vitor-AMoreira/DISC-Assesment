378f5ffe2bcf75a73f3320fc1a86fcac389012de
lbarrocamonteiro <barrocamonteiro@gmail.com>
2024-12-29 18:34:59 +0000

    Update utils.js

--- script/utils.js
+++ script/utils.js
@@ -20,26 +20,10 @@ function determineUserProfile(scores) {
 
 function getDevelopmentSuggestions(profileKey) {
     const suggestions = {
-        D: [
-            "Aprenda a exercitar a paciência em situações desafiadoras, buscando compreender diferentes perspetivas antes de agir."
-            "Dedique-se a ouvir os outros com atenção, valorizando as contribuições e criando um ambiente de colaboração."
-            "Trabalhe no desenvolvimento das suas habilidades de cooperação, equilibrando a liderança com o trabalho em equipa."
-        ],
-        I: [
-            "Treine a sua capacidade de foco, estabelecendo prioridades claras para manter a concentração em tarefas essenciais."
-            "Desenvolva uma abordagem analítica, complementando a sua criatividade com uma avaliação crítica mais estruturada."
-            "Encoraje-se a concluir tarefas com consistência, celebrando cada etapa como uma vitória no caminho para os seus objetivos."
-        ],
-        S: [
-            "Adapte-se melhor às mudanças, aceitando a imprevisibilidade como uma oportunidade para aprender e crescer."
-            "Exercite a assertividade nas suas comunicações, garantindo que as suas ideias sejam ouvidas de forma clara e respeitosa."
-            "Explore novas formas de pensar e abordar problemas, ampliando a sua zona de conforto com curiosidade e coragem."
-        ],
-        C: [
-            "Cultive uma maior flexibilidade, aprendendo a adaptar-se a situações fora dos padrões habituais e abraçando a mudança."
-            "Estimule o pensamento criativo, permitindo-se experimentar ideias inovadoras sem medo de errar."
-            "Abra-se a opções e abordagens diferentes, valorizando perspetivas alternativas para enriquecer as suas soluções."
-        ]
+        D: ["Aprenda a exercitar a paciência em situações desafiadoras, buscando compreender diferentes perspetivas antes de agir.", "Dedique-se a ouvir os outros com atenção, valorizando as contribuições e criando um ambiente de colaboração.","Trabalhe no desenvolvimento das suas habilidades de cooperação, equilibrando a liderança com o trabalho em equipa."],
+        I: ["Treine a sua capacidade de foco, estabelecendo prioridades claras para manter a concentração em tarefas essenciais.", "Desenvolva uma abordagem analítica, complementando a sua criatividade com uma avaliação crítica mais estruturada.", "Encoraje-se a concluir tarefas com consistência, celebrando cada etapa como uma vitória no caminho para os seus objetivos."],
+        S: ["Adapte-se melhor às mudanças, aceitando a imprevisibilidade como uma oportunidade para aprender e crescer.", "Exercite a assertividade nas suas comunicações, garantindo que as suas ideias sejam ouvidas de forma clara e respeitosa.", "Explore novas formas de pensar e abordar problemas, ampliando a sua zona de conforto com curiosidade e coragem."],
+        C: ["Cultive uma maior flexibilidade, aprendendo a adaptar-se a situações fora dos padrões habituais e abraçando a mudança.", "Estimule o pensamento criativo, permitindo-se experimentar ideias inovadoras sem medo de errar.", "Abra-se a opções e abordagens diferentes, valorizando perspetivas alternativas para enriquecer as suas soluções."]
     };
 
     return suggestions[profileKey] || ["Nenhuma sugestão disponível"];
